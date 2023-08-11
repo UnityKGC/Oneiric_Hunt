ee7d44a12d07c9d459ac4f2ef560202ed33fcaad
UnityKGC <114383066+UnityKGC@users.noreply.github.com>
2023-08-11 11:13:22 +0900

    QTE UI DOTween 구현
    ㅇㅇ

--- Assets/Scripts/UI/QTEUI.cs
+++ Assets/Scripts/UI/QTEUI.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 
 public class QTEUI : MonoBehaviour
 {
     public RectTransform _qteUI;
+    public RectTransform _qteFront;
     public TextMeshProUGUI _qteText;
     void Awake()
     {
         UIManager._instacne._qtePosEvt -= SetUIPos;
         UIManager._instacne._qtePosEvt += SetUIPos;
+    }
+    private void OnEnable()
+    {
+        _qteFront.DOScale(0, 2f).SetUpdate(true);
     }
     /*
     private void OnEnable() // 활성화 될 시, QTEUI를 1. 랜덤한 위치에 생성시킨다. 2. 지정된 위치에 생성시킨다.
@@ -18,10 +24,13 @@ public class QTEUI : MonoBehaviour
         
     }
     */
-    void SetUIPos(Vector2 vec)
+    void SetUIPos(Vector2 vec) // 위치를 받은 후, _qteUI를 해당 위치로 보낸다.
     {
-        // 위치를 받은 후, _qteUI를 해당 위치로 보낸다.
         _qteUI.anchoredPosition = vec;
+    }
+    private void OnDisable()
+    {
+        _qteFront.DOScale(1, 0f).SetUpdate(true);
     }
     private void OnDestroy()
     {
