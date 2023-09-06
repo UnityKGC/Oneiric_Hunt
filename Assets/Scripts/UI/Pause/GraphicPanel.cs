a1b919e5f0400ebbb1426daa6d158239330b1f0e
UnityKGC <114383066+UnityKGC@users.noreply.github.com>
2023-09-06 20:51:23 +0900

    PauseUI 리소스 적용 및 Text추가
    현재 해상도, 화면크기, 품질의 Text를 추가
    버튼 리소스 추가(변경예정)

--- Assets/Scripts/UI/Pause/GraphicPanel.cs
+++ Assets/Scripts/UI/Pause/GraphicPanel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GraphicPanel : MonoBehaviour
@@ -17,6 +18,10 @@ public class GraphicPanel : MonoBehaviour
 
     bool _isFullScreen = true;
 
+    [SerializeField] TextMeshProUGUI _nowResolution;
+    [SerializeField] TextMeshProUGUI _nowScreen;
+    [SerializeField] TextMeshProUGUI _nowGraphic;
+
     [SerializeField] int _resolutionNum = 0; // 해상도 변경 감지를 위해 사용하는 변수 => 현재 index
     [SerializeField] int _screenNum = 0; // 화면 변경 감지를 위해 사용하는 변수 => 현재 index
 
@@ -32,8 +37,8 @@ public class GraphicPanel : MonoBehaviour
     public void ClickScreenBtn(int idx) // 화면 변경
     {
         _graphicState = ChangeState.Screen;
-        ChangeScreen(idx);
-        CheckScreen(idx);
+        ChangeScreen(idx); // 화면 변경 적용
+        CheckScreen(idx); // 이전의 값과 다른지 확인
     }
 
     void CheckResoultion(int idx) // 버튼을 눌러 받은 idx가 기존의 Num와 다르면, Num으로 등록한다.
@@ -47,7 +52,7 @@ public class GraphicPanel : MonoBehaviour
             UIManager._instacne.SetPopupUI(_checkUI); // 재확인 Popup 호출
         }
     }
-    void CheckScreen(int idx)
+    void CheckScreen(int idx) // 이전의 값과 다른지 확인 => 다르면 재확인 ui 호출
     {
         if(_screenNum != idx)
         {
@@ -58,11 +63,11 @@ public class GraphicPanel : MonoBehaviour
             UIManager._instacne.SetPopupUI(_checkUI); // 재확인 Popup 호출
         }
     }
-    public void ClickAgreeBtn() // 예를 누르면 그대로 UI만 닫아, 변경한 설정을 유지한다
+    public void ClickAgreeBtn() // _checkUI => 예를 누르면 그대로 UI만 닫아, 변경한 설정을 유지한다
     {
         UIManager._instacne.ClosePopupUI();
     }
-    public void ClickDisagreeBtn() // 아니오를 누르면, 기존에 설정Num을 이용하여 이전 상태로 되돌린 후, UI를 닫는다.
+    public void ClickDisagreeBtn() // _checkUI => 아니오를 누르면, 기존에 설정Num을 이용하여 이전 상태로 되돌린 후, UI를 닫는다.
     {
         switch (_graphicState)
         {
@@ -84,15 +89,19 @@ public class GraphicPanel : MonoBehaviour
         {
             case 0:
                 Screen.SetResolution(800, 600, _isFullScreen);
+                _nowResolution.text = "800x600";
                 break;
             case 1:
                 Screen.SetResolution(1280, 1024, _isFullScreen);
+                _nowResolution.text = "1280x1024";
                 break;
             case 2:
                 Screen.SetResolution(1920, 1080, _isFullScreen);
+                _nowResolution.text = "1920x1080";
                 break;
             case 3:
                 Screen.SetResolution(2560, 1440, _isFullScreen);
+                _nowResolution.text = "2560x1440";
                 break;
         }
     }
@@ -103,16 +112,34 @@ public class GraphicPanel : MonoBehaviour
             case 0:
                 Screen.fullScreenMode = FullScreenMode.Windowed;
                 _isFullScreen = false;
+                _nowScreen.text = "창화면";
                 break;
 
             case 1:
                 Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
                 _isFullScreen = true;
+                _nowScreen.text = "전체화면 ";
                 break;
         }
     }
     public void ClickGraphicBtn(int idx) // 이건 재확인 UI 하지 말자.
     {
         QualitySettings.SetQualityLevel(idx);
+        switch(idx)
+        {
+            case 0:
+                _nowGraphic.text = "저화질";
+                break;
+            case 1:
+                _nowGraphic.text = "중화질";
+                break;
+            case 2:
+                _nowGraphic.text = "고화질";
+                break;
+        }
+    }
+    public void ClickExitButton()
+    {
+        UIManager._instacne.AllClosePopupUI(); // 모든 PopupUI 닫기
     }
 }
