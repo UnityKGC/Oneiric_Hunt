4cb9bb31b3d18269f41341c27828f1481b250475
UnityKGC <114383066+UnityKGC@users.noreply.github.com>
2023-09-15 09:01:33 +0900

    로딩화면 구현
    SceneManagerEX에 로딩화면 로직을 구현
    로딩화면(이미지) 생성

--- Assets/Scripts/Managers/SceneManagerEX.cs
+++ Assets/Scripts/Managers/SceneManagerEX.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class SceneManagerEX : MonoBehaviour
 {
     public static SceneManagerEX _instance;
 
+    [SerializeField] GameObject[] _images; // 로딩 이미지 배열
     public enum SceneType
     {
         None = -1,
@@ -27,7 +28,13 @@ public class SceneManagerEX : MonoBehaviour
     {
         _instance = this;
     }
-
+    private void Start()
+    {
+        foreach(GameObject obj in _images) // 로딩이미지 전부 비활성화.
+        {
+            obj.SetActive(false);
+        }
+    }
     public SceneType NowScene { get { return _nowScene; } set { _nowScene = value; } }
 
     private SceneType _nowScene = SceneType.None;
@@ -36,11 +43,8 @@ public class SceneManagerEX : MonoBehaviour
 
     public void LoadScene(SceneType scene)
     {
-        SceneManager.LoadScene((int)scene);
-    }
-    public void LoadScene(int idx)
-    {
-        SceneManager.LoadScene(idx);
+        StartCoroutine(LoadSceneAsync(scene));
+        //SceneManager.LoadScene((int)scene);
     }
     public void EnablePotal(SceneType sceneType, PortalType portalType)
     {
@@ -50,6 +54,24 @@ public class SceneManagerEX : MonoBehaviour
             case SceneType.FirstDreamScene:
                 _F_D_S.EnablePortal(portalType);
                 break;
+        }
+    }
+    IEnumerator LoadSceneAsync(SceneType scene) // sceneID를 SceneType으로 변환 후 받는다.
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync((int)scene);
+
+        int idx = Random.Range(0, 3);
+
+        GameObject loadImg = _images[idx];
+
+        loadImg.SetActive(true); // 로딩화면을 활성화 시킨다.
+        Image fillimg = loadImg.transform.GetChild(0).GetComponent<Image>(); // 0번째가 Fillimg
+
+        while (!operation.isDone) // 프로세스가 완료되지 않으면 반복
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f); // 진행값(0.dd)을 0.9로 나눠, 진행상태를 Value에 넣는다
+            fillimg.fillAmount = progressValue; // 그 value를 fill에 기입한다.
+            yield return null; // null 리턴
         }
     }
 }
