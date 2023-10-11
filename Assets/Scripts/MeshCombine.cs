6b24bd9ede3bea56bbc56ce95daf7fc23fa1e15e
UnityKGC <114383066+UnityKGC@users.noreply.github.com>
2023-10-11 15:49:24 +0900

    첫번째 Scene 최적화
    Mesh를 봐, 폴리곤이 쓸데없이 많은 철조망과 자동차 제거.
    WindMill 스킬에서, 시각적으로 눈에 띄지 않지만, 폴리곤이 많은 파티클 비활성화.

--- Assets/Scripts/MeshCombine.cs
+++ Assets/Scripts/MeshCombine.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class MeshCombine : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] MeshFilter[] meshFilters = null;
+    [SerializeField] CombineInstance[] combine = null;
     void Start()
     {
-        
-    }
+        meshFilters = GetComponentsInChildren<MeshFilter>();
+        combine = new CombineInstance[meshFilters.Length];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < meshFilters.Length; i++)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            // Optionally, you can disable the original renderers of the child objects
+            meshFilters[i].gameObject.SetActive(false);
+        }
+
+        // Create a new mesh to hold the combined mesh data
+        Mesh combinedMesh = new Mesh();
+
+        // Combine the meshes into the new mesh
+        combinedMesh.CombineMeshes(combine, true, true);
+
+        // Optionally, optimize the mesh for better performance
+        combinedMesh.Optimize();
+
+        // Create a new GameObject to hold the combined mesh
+        GameObject combinedObject = new GameObject("CombinedRoads");
+
+        // Add MeshFilter and MeshRenderer components to the new GameObject
+        MeshFilter meshFilter = combinedObject.AddComponent<MeshFilter>();
+        MeshRenderer meshRenderer = combinedObject.AddComponent<MeshRenderer>();
+
+        // Assign the combined mesh to the MeshFilter component
+        meshFilter.mesh = combinedMesh;
+
+        // Optionally, assign a material to the MeshRenderer component
+        meshRenderer.material = meshFilters[0].GetComponent<MeshRenderer>().sharedMaterial;
+
+        combinedObject.isStatic = true; // 정적으로 했는데, 의미가 있나? 오클루전은 Bake를 해야 진행되는데, 그래도 일단은 static으로 설정.
     }
 }
