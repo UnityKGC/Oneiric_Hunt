5ddd8f9bf7e343e1802c1dcfed06811771fba406
UnityKGC <114383066+UnityKGC@users.noreply.github.com>
2023-10-17 15:48:30 +0900

    물리 문제 해결
    1. 벽 뚫기
      - DB플레이어의 Rigidbody의 Interpolate와 Colision Detection을 각 Extrapolate와 Continuous로 변경
    2. 구르기 스킬 사용 시 벽 뚫기
       - Ray를 쏘아, 벽에 Hit되면, 제자리에서 구르도록 변경

--- Assets/Scripts/Skill/Dodge.cs
+++ Assets/Scripts/Skill/Dodge.cs
@@ -8,11 +8,16 @@ public class Dodge : MonoBehaviour
 
     float _moveSpd = 15f;
     private WaitForEndOfFrame _returnTime = new WaitForEndOfFrame();
+    int _mask = 1 << 13;
+
+    Transform _player;
     public void Init(SkillScriptable scriptable, Vector3 playerPos, Quaternion playerRot)
     {
         _scriptable = scriptable;
 
         _scriptable._isAble = false;
+
+        _player = transform.parent;
 
         StartCoroutine(StartDodgeCo(playerRot));
     }
@@ -21,8 +26,23 @@ public class Dodge : MonoBehaviour
         float time = _scriptable._durationTime;
         while(time > 0f)
         {
+            
             Vector3 dir = playerRot * Vector3.forward;
-            transform.parent.position += dir * _moveSpd * Time.deltaTime; // 부모가 플레이어니까 이렇게 하긴 했는데.. 이게 맞나...? KGC
+            Vector3 RayDir = new Vector3(dir.x, 1.5f, dir.z);
+
+            Ray ray;
+            RaycastHit hit;
+
+            Debug.DrawRay(_player.position, RayDir, Color.blue);
+
+            if (Physics.Raycast(_player.position, RayDir, _mask))
+            {
+                //_player.position += dir * 0 * Time.deltaTime;
+            }
+            else
+            {
+                _player.position += dir * _moveSpd * Time.deltaTime; // 부모가 플레이어니까 이렇게 하긴 했는데.. 이게 맞나...? KGC
+            }
             time -= Time.deltaTime;
             yield return _returnTime;
         }
