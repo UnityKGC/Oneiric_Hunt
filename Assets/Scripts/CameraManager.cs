93227a78b45120d2f9f9308eb8eed886b52d9198
UnityKGC <114383066+UnityKGC@users.noreply.github.com>
2023-12-07 14:10:39 +0900

    첫번째 Scene 리소스 변경 및 재할당
    리소스 변경 및 오브젝트(퀘스트 물품)등의 스크립트 재할당

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -83,12 +83,15 @@ public class CameraManager : MonoBehaviour
     {
         ChangeCam(CameraType.TalkCam);
 
+        /* TalkCam을 그냥 위치를 지정하고 진행하도록 변경
+         * 
         Vector3 pos = fromObj.transform.position + (fromObj.transform.forward * -3f) + (fromObj.transform.right * 2f);
         pos.y = 3.5f;
 
         _nowCam.ForceCameraPosition(pos, Quaternion.identity);
         Vector3 vec = new Vector3(toObj.transform.position.x, toObj.transform.position.y + 1, toObj.transform.position.z);
         _nowCam.transform.rotation = Quaternion.LookRotation(vec - _nowCam.transform.position, Vector3.up);
+        */
         //_nowCam.LookAt = toObj.transform;
     }
 
